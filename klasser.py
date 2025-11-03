a449d50210239270698fe0a8dbd461502ed4464f
Tehzeeb <tehzeebabbas43@gmail.com>
2025-11-03 21:38:00 +0100

    läggt till special metod för alla subklasser och lagt till Arena class

--- klasser.py
+++ klasser.py
@@ -28,6 +28,16 @@ class Mage(karaktär):
         print(f"{self.namn} använder magiskt attack mot {other.namn}  för {damage} skada")
         other.take_damage(damage)
 
+    def special(self, other):
+        if self.mana >= 3:
+            self.mana -= 3
+            skada = self.attackkraft + 6
+            print(f"{self.namn} använder special (mana kvar {self.mana}) för {skada} skada.")
+            other.take_damage(skada)
+        else:
+            print(f"{self.namn} har inte tillräckligt med mana. Utför vanlig attack istället.")
+            self.attack(other)
+
 class Ranger(karaktär):
     def __init__(self, namn, hälsa, attackkraft, energy):
         super().__init__(namn, hälsa, attackkraft)
@@ -37,6 +47,16 @@ class Ranger(karaktär):
         damage = self.attackkraft + 1
         print(f"{self.namn} skjuter pil mot {other.namn} för {damage} skada")
         other.take_damage(damage)
+    
+    def special(self, other):
+        if self.energy >= 2:
+            self.energy -= 2
+            skada = self.attackkraft + 4
+            print(f"{self.namn} använder special (energy kvar {self.energy}) för {skada} skada.")
+            other.take_damage(skada)
+        else:
+            print(f"{self.namn} har inte tillräckligt med energy. Utför vanlig attack istället.")
+            self.attack(other)
 
 class Warrior(karaktär):
     def __init__(self, namn, hälsa, attackkraft, stamina):
@@ -48,8 +68,30 @@ class Warrior(karaktär):
         print(f"{self.namn} svingar svärd mot {other.namn} för {damage} skada")
         other.take_damage(damage)
 
+    def special(self, other):
+        if self.stamina >= 2:
+            self.stamina -= 2
+            skada = self.attack_power + 5
+            print(f"{self.namn} använder special (stamina kvar {self.stamina}) för {skada} skada.")
+            other.take_damage(skada)
+        else:
+            print(f"{self.namn} har inte tillräckligt med stamina. Utför vanlig attack istället.")
+            self.attack(other)
+
+class Arena:
+    def __init__(self, spelare1, spelare2):
+        self.spelare1 = spelare1
+        self.spelare2 = spelare2
+
+    def fight(self):
+        attacker = self.spelare1
+        defender = self.spelare2
+        runda = 1
+        while attacker.is_alive() and defender.is_alive():
+            print(f"\nRunda {runda}")
+
 mage = Mage("Merlin", 20, 3, mana = 10)
-ranger = Ranger("Robin", 18, 4, energy = 8)
+ranger = Ranger("Robin", 181, 4, energy = 8)
 warrior = Warrior("Thor", 22, 2, stamina = 12)
 
 mage.attack(ranger)
